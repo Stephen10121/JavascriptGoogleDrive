28de01c63c3c5309d17221e363af546a1a76b5df
Stephen Gruzin <stephengrzn@gmail.com>
2021-12-31 15:51:09 -0800

    Started work on main page layout and created the color pallete and styling

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import React, { useState, useEffect, useRef } from "react";
 import { getCookie } from "./Cookie";
 import { files } from "./getFiles";
+import './styles/mainPage.css';
 
 const HomePage = (props) => {
     const [user] = useState(JSON.parse(window.localStorage.getItem("user")));
@@ -20,6 +21,7 @@ const HomePage = (props) => {
     const onStartup = useRef(() => {});
     onStartup.current = () => {
         loadUserData();
+        console.log(user);
         files(userId).then((data) => {
             if (data !== "error") {
                 setFiles(data.data.data);
@@ -35,8 +37,37 @@ const HomePage = (props) => {
 
     return (
     <div className="HomePage">
-        Hello {user.rname}!
-        <Link to="/logout">Logout</Link>
+        <div className="taskbar">
+            <div className="profile">
+                <button title="Your Profile" className="profile-button">
+                    profile
+                </button>
+            </div>
+            <div className="upload-icon">
+                <button title="Upload a file or folder">
+                    <img src="./icons/upload.svg" alt="Upload"/>
+                </button>
+            </div>
+            <div className="shared-icon">
+                <button title="Shared With Me">
+                    <img src="./icons/shared.svg" alt="Shared with me"/>
+                </button>
+            </div>
+            <div className="logout-link">
+                <Link to="/logout">Logout</Link>
+            </div>
+        </div>
+        <div className="files-box">
+            <div className="name-show">
+                <p className="name-show-p">{user.rname}</p>
+            </div>
+            <div>
+                files
+            </div>
+        </div>
+        <div className="main-files">
+            main files
+        </div>
     </div>
     );
 }
