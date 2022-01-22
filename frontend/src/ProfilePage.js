bce40b4ec3eabd859b10e9369a37e341aed7be4d
Stephen Gruzin <stephengrzn@gmail.com>
2022-01-21 16:57:29 -0800

    Working on profile page styling and added a checkbox style

--- frontend/src/ProfilePage.js
+++ frontend/src/ProfilePage.js
@@ -1,9 +1,29 @@
 //import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import "./styles/ProfilePage.css";
 
 const ProfilePage = (props) => {
   return (
     <div className="ProfilePage">
-      profile page
+        <div className="taskbar">
+            <div className="home-icon">
+                <Link to="/" title="Go Home">
+                    <img src="./icons/house.svg" alt="house icon"/>
+                </Link>
+            </div>
+            <div className="shared-icon">
+                <button title="Shared With Me">
+                    <img src="./icons/shared.svg" alt="Shared with me"/>
+                </button>
+            </div>
+            <div className="logout-link">
+                <Link to="/logout">Logout</Link>
+            </div>
+        </div>
+        <div className="profile-settings">
+            <input className="checkbox" type='checkbox'/>
+            <input className="checkbox" type='checkbox'/><input className="checkbox" type='checkbox'/><input className="checkbox" type='checkbox'/>
+        </div>
     </div>
   );
 }
