fdbce9c0e112258d17013c01ab031537d6f83f00
Stephen Gruzin <stephengrzn@gmail.com>
2022-03-22 16:58:47 -0700

    Working on right click in folder section

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -46,9 +46,40 @@ const HomePage = (props) => {
         changeFiles(newFolders);
     }
 
+    const rightClickListener = () => {
+        const contextMenu = document.querySelector(".right-click");
+        const folderBox = document.querySelector("#folder-box");
+
+        folderBox.addEventListener("contextmenu", (e) => {
+            e.preventDefault();
+            let x = e.pageX, y = e.pageY,
+            winWidth = window.innerWidth,
+            winHeight = window.innerHeight,
+            cmWidth = contextMenu.offsetWidth,
+            cmHeight = contextMenu.offsetHeight;
+            x = x > winWidth - cmWidth ? winWidth - cmWidth : x;
+            y = y > winHeight - cmHeight ? winHeight - cmHeight : y;
+            contextMenu.style.left = `${x}px`;
+            contextMenu.style.top = `${y}px`;
+            contextMenu.style.visibility = "visible";
+        });
+
+        document.addEventListener("click", (e) => {
+            if (contextMenu.style.visibility !== "visible") {return;}
+            const pos = {x: parseInt(contextMenu.style.left.replace("px", "")), y: parseInt(contextMenu.style.top.replace("px", "")), width: contextMenu.offsetWidth, height: contextMenu.offsetHeight}
+            if ((pos.x + pos.width) >= e.pageX && e.pageX >= pos.x) {
+                if ((pos.y + pos.height) >= e.pageY && e.pageY >= pos.y) {
+                    return;
+                }
+            }
+            contextMenu.style.visibility = "hidden";
+        });
+    }
+
     const onStartup = useRef(() => {});
     onStartup.current = () => {
         loadUserData();
+        rightClickListener();
     }
 
     useEffect(() => {
@@ -61,6 +92,14 @@ const HomePage = (props) => {
 
     return (
     <div className="HomePage">
+        <div className="right-click">
+            <ul>
+                <li><button>New Folder</button></li>
+                <li><button>Delete Folder</button></li>
+                <li><button>Move Folder</button></li>
+                <li><button>Share Folder</button></li>
+            </ul>
+        </div>
         <div className="taskbar">
             <div className="profile">
                 <Link to="/profile" title="Your Profile" className="profile-button">
@@ -79,7 +118,7 @@ const HomePage = (props) => {
                 <Link to="/logout">Logout</Link>
             </div>
         </div>
-        <div className="files-box">
+        <div className="files-box" id="folder-box">
             <div className="name-show">
                 <p className="name-show-p">{user.userData.usersRName}</p>
             </div>
